9ef64b30c72334da085f1cd622f96bda5345c2c1
unknown <dastagir2k@gmail.com>
2024-12-23 19:36:05 +0530

    router changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateTrip from './create-trip';
 import { Toaster } from './components/ui/sonner';
 import Headers from './components/custom/Header';
@@ -8,23 +8,23 @@ import Signup from './components/custom/Signup';
 import View from './TripDetails/View';
 import Profile from './components/custom/Profile';
 import SharableProfile from './components/custom/SharableProfile';
-
-const router = createBrowserRouter([
-  { path: '/', element: <Headers /> },
-  { path: '/create-trip', element: <CreateTrip /> },
-  { path: '/login', element: <Login /> },
-  { path: '/signup', element: <Signup /> },
-  { path: '/view', element: <View /> },
-  { path: '/profile', element: <Profile /> },
-  { path: '/profile/:email', element: <SharableProfile /> },
-]);
+import Hero from './components/custom/Hero';
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
+    <Router>
+      <Headers />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/create-trip" element={<CreateTrip />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/view" element={<View />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:email" element={<SharableProfile />} />
+      </Routes>
       <Toaster />
-    </>
+    </Router>
   );
 }
 
